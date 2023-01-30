fb551a44556824886254b11bc757306004003823
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-01-30 22:26:21 +0300

    Abstarct classes

--- Lessons/ONEMLI - Abstract Classes/untitled/src/Main.java
+++ Lessons/ONEMLI - Abstract Classes/untitled/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // abstract classlar normal classlar ile ayni kurallara sahiptir.
+        // abstract operasyonu icin abstract belirtilmeli
+        // abstract sinif asla newlenemez!! islemlerinin ezilmesi gerekir
+        // referans tutabilir.
+
+        WomanGameCalculator womanGameCalculator = new WomanGameCalculator();
+        womanGameCalculator.hesapla();
+        womanGameCalculator.gameOver();
+
+        GameCalculator gameCalculator = new ManGameCalculator();
+        gameCalculator.hesapla();
+
     }
 }
