588bf47405ad831e4a15c15c63434168fba18e56
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-23 13:43:30 +0300

    Banker System

--- myJavaProjects/bankerSystem/untitled/src/Main.java
+++ myJavaProjects/bankerSystem/untitled/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your 'Name', 'Surname', 'accountNumber' and 'password' to access your Bank account:");
+        String name=sc.nextLine();
+        String surname=sc.nextLine();
+        int accountNumber= Integer.parseInt(sc.nextLine());
+        String password=sc.nextLine();
+
+
+
+        baseCustomerManager baseCustomerManager = new baseCustomerManager();
+        baseCustomerManager.setName(name);
+        baseCustomerManager.setSurname(surname);
+        baseCustomerManager.setPassword(password);
+        baseCustomerManager.setAccountNumber(accountNumber);
+
+
+        baseCustomerManager.getName();
+        baseCustomerManager.getSurname();
+        baseCustomerManager.getAccountNumber();
+        baseCustomerManager.getPassword();
+
+
     }
 }
