3ba427b101e89409c4b4e7653af5ae65d7da9c1a
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-23 16:49:46 +0300

    Creating our own packages and using it

--- Lessons/Packages/untitled/src/Main.java
+++ Lessons/Packages/untitled/src/Main.java
@@ -1,4 +1,8 @@
 //built-in packages
+// import matematik.*;
+// yukaridaki * isaretli import islemi paket icerisindeki tum classlari kullanmamiza olanak saglar.
+import matematik.DortIslem;
+import matematik.Logaritma;
 
 import java.util.Scanner;
 public class Main {
@@ -8,5 +12,12 @@ public class Main {
 
         String Isim = scanner.nextLine();
         System.out.println("Merhaba " + Isim);
+
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.Topla(2,3);
+
+        Logaritma logaritma = new Logaritma();
+        logaritma.LogaritmaHesapla();
+
     }
 }
---
+++ Lessons/Packages/untitled/src/matematik/Logaritma.java
@@ -0,0 +1,7 @@
+package matematik;
+
+public class Logaritma {
+    public double LogaritmaHesapla(){
+        return 1;
+    }
+}
