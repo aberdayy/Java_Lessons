15d1df2b80b260ad4b25ceb631e4f78321d71207
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-23 16:41:58 +0300

    Packages Created and Used

--- Lessons/Packages/untitled/src/Main.java
+++ Lessons/Packages/untitled/src/Main.java
@@ -1,5 +1,12 @@
+//built-in packages
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adinizi giriniz : ");
 
+        String Isim = scanner.nextLine();
+        System.out.println("Merhaba " + Isim);
     }
 }
