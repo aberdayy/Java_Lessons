cc2965cb473133ddc5a7acadd5a8fd9a843d393b
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-26 15:24:17 +0300

    Exceptions Demo | Checked Exceptions

--- Lessons/Error Managment/ErrorManagmentDemo/untitled/src/Main.java
+++ Lessons/Error Managment/ErrorManagmentDemo/untitled/src/Main.java
@@ -1,5 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader= null;
+        int total = 0;
+
+        try {
+            reader = new BufferedReader(new FileReader("C:\\Users\\atabe\\IdeaProjects\\Java_Lessons\\Lessons\\Error Managment\\ErrorManagmentDemo\\untitled\\src\\sayilar.txt"));
+            String line = null;
+            while ( (line = reader.readLine() )!= null){
+                total += Integer.valueOf(line); //String sayi degerini integer yapar
+            }
+            System.out.println("Toplam = " + total);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        }finally {
+            try {
+                reader.close();
+
+            }catch (Exception exception){
+                System.out.println(exception);
+            }
+
+        }
+
+
     }
 }
