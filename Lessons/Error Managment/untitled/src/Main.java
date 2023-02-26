408d47aecfb323a8df88853e914de038512ba438
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-26 14:41:22 +0300

    Error Management  | Try Catch Finally

--- Lessons/Error Managment/untitled/src/Main.java
+++ Lessons/Error Managment/untitled/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try
+        {
+            int[] sayilar = new int[]{1, 2, 3};
+            System.out.println(sayilar[5]);
+
+        } catch (Exception exception){
+            System.out.println("Hata Olustu! Gelen hata =>  " + exception);
+        }finally {
+            System.out.println("Ben her turlu calisirim");
+        }
+
+        System.out.println("Bu kod blogu hata yonetimi sayesinde calismaya devam eder");
+
     }
 }
