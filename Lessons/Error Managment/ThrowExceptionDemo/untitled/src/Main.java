08df712b620da16d6d9feb9cb4ee3d0157530023
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-26 15:54:51 +0300

    Throw Exceptions Demo | Creating our own exception

--- Lessons/Error Managment/ThrowExceptionDemo/untitled/src/Main.java
+++ Lessons/Error Managment/ThrowExceptionDemo/untitled/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        AccountManager manager = new AccountManager();
+        System.out.println("Hesap bakiyesi = "+manager.getBalance() + " TRY");
+        manager.deposit(100);
+        System.out.println("Hesap bakiyesi = "+manager.getBalance() + " TRY");
+
+        try {
+            manager.withdraw(90);
+        } catch (BalanceInsufficentException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Hesap bakiyesi = "+manager.getBalance() + " TRY");
+        try {
+            manager.withdraw(20);
+        } catch (BalanceInsufficentException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Hesap bakiyesi = "+manager.getBalance() + " TRY");
     }
 }
