be5936e9b5bdb3cfc32e4f82a731e5558bd80cee
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-28 21:07:50 +0300

    Generics Method and Usage

--- Lessons/Generics/untitled/src/Main.java
+++ Lessons/Generics/untitled/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+  // ==========Bu bir array listtir.
+  //Generic class lar array listlerin ilkel halidir. Kendi arraylistimizi generic classlar sayesinde yazabiliriz.
+        ArrayList<String> sehirler = new ArrayList<String>();
+        sehirler.add("Ankara");
+        sehirler.add("Istanbul");
+        sehirler.add("Izmir");
+*/
+        MyList<String> sehirler = new MyList<>();
+        sehirler.add("Ankara");
+        sehirler.add("Istanbul");
+        // sehirler.add(1); //Hata dondurur cunku tipki arraylistte oldugu gibi String tipinde oldugunu belirttik.
+
+        //tipki arraylist gibi bunun benzeri islemlerde yapilabilir.
+        MyList<Customer> musteriler = new MyList<>();
+        musteriler.add(new Customer());
+        musteriler.add(new Customer());
+
+
+
+
     }
 }
