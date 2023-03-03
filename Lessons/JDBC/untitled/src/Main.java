8102dbdd6a53a7dab46528cf2da6ce7cbffe7b0b
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-03-03 15:05:46 +0300

    Java database connection |Parametreli SQL Insert ifadesi kullanimi

--- Lessons/JDBC/untitled/src/Main.java
+++ Lessons/JDBC/untitled/src/Main.java
@@ -9,7 +9,14 @@ public class Main {
         ResultSet resultSet;
         try {
             connection = helper.getConnection();
-            statement= connection.prepareStatement("insert into city(Name,CountryCode,District,Population) values ('Duzce','TUR','Duzce','50000')");
+            String sql = "insert into city(Name,CountryCode,District,Population) values (?,?,?,?)";
+            statement= connection.prepareStatement(sql);
+
+            statement.setString(1,"Duzce 2");
+            statement.setString(2,"TUR");
+            statement.setString(3,"Marmara");
+            statement.setInt(4,99920);
+
             int result = statement.executeUpdate();
             System.out.println("Kayit eklendi ");
 
@@ -50,4 +57,24 @@ public class Main {
             }
 
         }
-    }
+        public static void insertToDbManual() throws SQLException{
+            Connection connection = null;
+            DbHelper helper = new DbHelper();
+            PreparedStatement statement = null;
+            ResultSet resultSet;
+            try {
+                connection = helper.getConnection();
+                statement= connection.prepareStatement("insert into city(Name,CountryCode,District,Population) values ('Duzce','TUR','Duzce','50000')");
+                int result = statement.executeUpdate();
+                System.out.println("Kayit eklendi ");
+
+            } catch (SQLException exception) {
+                helper.showErrorMessage(exception);
+            } finally {
+                statement.close();
+                connection.close();
+            }
+
+
+        }
+}
