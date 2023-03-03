1d1a519a59fcc2adf1afaf0b29291eba0d3d1d49
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-03-03 14:54:02 +0300

    Java database connection | SQL Insert ifadesi kullanimi

--- Lessons/JDBC/untitled/src/Main.java
+++ Lessons/JDBC/untitled/src/Main.java
@@ -5,29 +5,49 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
         DbHelper helper = new DbHelper();
-        Statement statement;
+        PreparedStatement statement = null;
         ResultSet resultSet;
         try {
             connection = helper.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select * from country ");
-
-            ArrayList<Country> countries = new ArrayList<Country>();
-            while (resultSet.next()){
-                countries.add(new Country(
-                        resultSet.getString("Code"),
-                        resultSet.getString("Name"),
-                        resultSet.getString("Continent"),
-                        resultSet.getString("Region")));
-            }
-            System.out.println(countries.size());
-
+            statement= connection.prepareStatement("insert into city(Name,CountryCode,District,Population) values ('Duzce','TUR','Duzce','50000')");
+            int result = statement.executeUpdate();
+            System.out.println("Kayit eklendi ");
 
         } catch (SQLException exception) {
             helper.showErrorMessage(exception);
         } finally {
+            statement.close();
             connection.close();
         }
 
+
     }
-}
+        public static void selectFromDb() throws SQLException {
+            Connection connection = null;
+            DbHelper helper = new DbHelper();
+            Statement statement;
+            ResultSet resultSet;
+            try {
+                connection = helper.getConnection();
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery("select * from country ");
+
+                ArrayList<Country> countries = new ArrayList<Country>();
+                while (resultSet.next()){
+                    countries.add(new Country(
+                            resultSet.getString("Code"),
+                            resultSet.getString("Name"),
+                            resultSet.getString("Continent"),
+                            resultSet.getString("Region")));
+                }
+                System.out.println(countries.size());
+
+
+            } catch (SQLException exception) {
+                helper.showErrorMessage(exception);
+            } finally {
+                connection.close();
+            }
+
+        }
+    }
