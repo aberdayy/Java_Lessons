8093621f593fbad615a51bf7bc64a5bcf7a11cd6
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-03-03 14:40:45 +0300

    Java database connection | SQL Select ifadesi | Nesneleri resultSet icerisine atamak

--- Lessons/JDBC/untitled/src/Main.java
+++ Lessons/JDBC/untitled/src/Main.java
@@ -1,5 +1,33 @@
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+        Statement statement;
+        ResultSet resultSet;
+        try {
+            connection = helper.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from country ");
+
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (resultSet.next()){
+                countries.add(new Country(
+                        resultSet.getString("Code"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Continent"),
+                        resultSet.getString("Region")));
+            }
+            System.out.println(countries.size());
+
+
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        } finally {
+            connection.close();
+        }
+
     }
 }
