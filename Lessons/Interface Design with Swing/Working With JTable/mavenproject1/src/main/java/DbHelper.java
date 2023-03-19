7a9c4d0123724b8784fef73a30a6c8cc061a2d9b
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-03-19 15:06:07 +0300

    added

--- Lessons/Interface Design with Swing/Working With JTable/mavenproject1/src/main/java/DbHelper.java
+++ Lessons/Interface Design with Swing/Working With JTable/mavenproject1/src/main/java/DbHelper.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author atabe
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbHelper {
-    
+    private final String username = "root";
+    private final String pass = "admin";
+    private final String dbUrl = "jdbc:mysql://localhost:3306/world";
+    public Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(dbUrl,username,pass);
+    }
+    public void showErrorMessage(SQLException exception){
+        System.out.println("error => " + exception.getMessage());
+        System.out.println("Error code => " + exception.getErrorCode());
+    }
 }
